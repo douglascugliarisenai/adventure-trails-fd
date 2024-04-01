0de349a3fc364285b79971733735c93deda097ee
douglascugliaridev <douglas.cugliari@gmail.com>
2024-04-01 19:15:06 -0300

    Exibindo os Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CardTrilha from "./components/CardTrilla";
+import "./App.css";
 
 function App() {
 
@@ -6,25 +7,26 @@ function App() {
     {
       nomeTrilha: "Trilha da Costa da Lagoa",
       cidade: "Florianópolis",
-      estado: 'Santa Catarina',
+      estado: 'SC',
       duracao: 120,
       trajeto: 4,
-      dificuldade: "iniciante",
+      dificuldade: "Iniciante",
       tipoTrilha: "caminhada/trekking",
-      nomeCriador: "Douglas Panizza Cugliari dos Santos",
+      nomeCriador: "Douglas Santos",
       UrlImagem: "../src/assets/imagem-trilha-exemplo.jpg",
     },
   ];
 
   return (
-    <>
-      {
+    <div className="container">
+      <h1 className="titulo">Explore trilhas incríveis</h1>
+        {
         listaTrilhas.map((trilha, index) => (
            <CardTrilha dadosTrilha={trilha} key={index}/>
          ))
       }
-    </> 
+    </div>
   );
 }
 
-export default App;
+export default App
