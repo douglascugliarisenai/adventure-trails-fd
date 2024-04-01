213ce6b2676171ba0160cee9cc0b16be3dcc64ae
douglascugliaridev <douglas.cugliari@gmail.com>
2024-04-01 17:40:24 -0300

    Criando os PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import CardTrilha from "./components/CardTrilla"
+import CardTrilha from "./components/CardTrilla";
 
 function App() {
 
+  const listaTrilhas = [
+    {
+      nomeTrilha: "Trilha da Costa da Lagoa",
+      cidade: "Florianópolis",
+      estado: 'Santa Catarina',
+      duracao: 120,
+      trajeto: 4,
+      dificuldade: "iniciante",
+      tipoTrilha: "caminhada/trekking",
+      nomeCriador: "Douglas Panizza Cugliari dos Santos",
+      UrlImagem: "../src/assets/imagem-trilha-exemplo.jpg",
+    },
+  ];
+
   return (
     <>
-      <CardTrilha />
-    </>
-  )
+      {
+        listaTrilhas.map((trilha, index) => (
+           <CardTrilha dadosTrilha={trilha} key={index}/>
+         ))
+      }
+    </> 
+  );
 }
 
-export default App
+export default App;
