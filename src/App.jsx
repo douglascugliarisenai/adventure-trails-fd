193ef91b9eb75111dc49826442843dc50be0ca8b
douglascugliaridev <douglas.cugliari@gmail.com>
2024-04-07 21:03:11 -0300

    Crie um hook para utilizar o fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
+/* eslint-disable */
+
 import CardTrilha from "./components/CardTrilla";
 import "./App.css";
+import getJson from "./hooks/getJson";
+
+
 
 function App() {
 
-  const listaTrilhas = [
-    {
-      nomeTrilha: "Trilha da Costa da Lagoa",
-      cidade: "Florianópolis",
-      estado: 'SC',
-      duracao: 120,
-      trajeto: 4,
-      dificuldade: "Iniciante",
-      tipoTrilha: "caminhada/trekking",
-      nomeCriador: "Douglas Santos",
-      UrlImagem: "../src/assets/imagem-trilha-exemplo.jpg",
-    },
-  ];
-
+  // const cards = fetch("../public/data/cards.json").then((res) => res.json());
+  
+  const [cards, loading] = getJson("../public/data/cards.json");
+  
+  
   return (
     <div className="container">
       <h1 className="titulo">Explore trilhas incríveis</h1>
-        {
-        listaTrilhas.map((trilha, index) => (
-           <CardTrilha dadosTrilha={trilha} key={index}/>
-         ))
-      }
+        {loading && <h4>Carregando Cards...</h4>}
+
+        {!loading &&  cards !== null && (
+          cards.map((cards, index) => (
+            <CardTrilha dadosTrilha={cards} key={index}/>
+          ))
+        )}
+      
     </div>
   );
 }
